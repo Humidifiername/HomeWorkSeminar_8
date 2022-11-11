219e28422333e4e89a8aa12db288b2eaaecbda4e
Albert <albert.fedorov.99@bk.ru>
2022-11-11 15:32:04 +0300

    Сделали дз 62

---
+++ Task_62/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 62. Заполните спирально массив 4 на 4
+
+int[,] array = new int[4, 4];
+
+void FillArraySpiral(int[,] array, int n)
+{
+    int i = 0, j = 0;
+    int value = 1;
+    for (int e = 0; e < n * n; e++)
+    {
+        int k = 0;
+        do { array[i, j++] = value++; } while (++k < n - 1);
+        for (k = 0; k < n - 1; k++) array[i++, j] = value++;
+        for (k = 0; k < n - 1; k++) array[i, j--] = value++;
+        for (k = 0; k < n - 1; k++) array[i--, j] = value++;
+        ++i; ++j;
+        n = n < 2 ? 0 : n - 2;
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArraySpiral(array, 4);
+PrintArray(array);
